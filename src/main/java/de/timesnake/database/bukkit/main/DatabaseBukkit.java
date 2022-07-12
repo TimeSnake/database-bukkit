351f127fee4b6cb500dcc81eb2f0c40d1f845a1b
funzter <funzter@mail.timesnake.de>
2022-07-12 20:48:47 +0200

    changed to toml config

--- src/main/java/de/timesnake/database/bukkit/main/DatabaseBukkit.java
+++ src/main/java/de/timesnake/database/bukkit/main/DatabaseBukkit.java
@@ -3,26 +3,28 @@ package de.timesnake.database.bukkit.main;
 import de.timesnake.database.bukkit.file.Config;
 import de.timesnake.database.core.file.DatabaseNotConfiguredException;
 import de.timesnake.database.util.Database;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 
 public class DatabaseBukkit extends JavaPlugin {
+
+    public static void disconnect() {
+        Database.getInstance().close();
+    }
 
     @Override
     public void onEnable() {
         Config config = new Config();
         config.onEnable();
-        config.loadProxy();
         try {
             Database.getInstance().connect(config);
         } catch (DatabaseNotConfiguredException e) {
-            System.out.println(e.getMessage());
+            Bukkit.getLogger().log(Level.INFO, e.getMessage());
         }
-        System.out.println("[Database] Databases loaded successfully!");
+        Bukkit.getLogger().log(Level.INFO, "[Database] Databases loaded successfully!");
 
-    }
-
-    public static void disconnect() {
-        Database.getInstance().close();
     }
 }
